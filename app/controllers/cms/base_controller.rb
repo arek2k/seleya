ef3bb175310cc2320fb3663059045ee922df7282
Grzegorz Bizon <grzesiek@ntsn.pl>
2014-03-29 18:11:19 +0100

    Add Admin model with auth, add index controller

--- app/controllers/cms/base_controller.rb
+++ app/controllers/cms/base_controller.rb
@@ -1,7 +1,7 @@
 class Cms::BaseController < ApplicationController
 
   layout 'cms'
-  before_filter :authenticate_user!
+  before_filter :authenticate_admin!
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Seleya::Application.routes.draw do
 
   devise_for :users
-  root "cms/index#index"
+  devise_for :admins
+
+  root "index#index"
 
   namespace :cms do
-        get "/" => "index#index", :as => "index"
+    get "/" => "index#index", :as => "index"
   end
 
 end
