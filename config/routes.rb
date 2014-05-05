4ba035645a6331afe7a0e8427813437435b3cb72
Grzegorz Bizon <grzesiek@ntsn.pl>
2014-05-05 12:28:47 +0200

    Remove cms/page/index controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Seleya::Application.routes.draw do
   devise_for :users, class_name: 'Cms::User'
   namespace :cms do
     get '/' => 'index#index', :as => 'index'
+    resources :page
   end
 
   root 'index#index'
