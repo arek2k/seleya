1fb2ba98c38f05fc9b828ac73497a6b6b05040a8
Grzegorz Bizon <grzesiek@ntsn.pl>
2014-05-06 09:32:49 +0200

    Add page crud sandbox

--- app/controllers/cms/pages_controller.rb
+++ app/controllers/cms/pages_controller.rb
@@ -3,7 +3,20 @@ class Cms::PagesController < Cms::BaseController
   def index
   end
 
+  def new
+    @page = Page.new
+  end
+
   def create
+    
+    page_params = params.require(:page).permit(:route)
+    @page = Page.new(page_params)
+    @page.save
+    redirect_to @page
+
+  end
+
+  def show
   end
 
 end
